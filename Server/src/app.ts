42e12b83c49a6bde1f57f277ab968a671a05cd9b
Matosoli <guilhermematosgto2015@outlook.com>
2023-09-21 17:29:35 -0300

    finish integration tests to the register user controller :robot:

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -3,7 +3,8 @@ import { router } from './routes/router';
 import cors from 'cors';
 
 export const app = express();
+
+
 app.use(cors())
 app.use(express.json());
 app.use(router);
-
--- Server/src/controllers/RegisterUserController.spec.ts
+++ Server/src/controllers/RegisterUserController.spec.ts
@@ -1,5 +1,18 @@
 import { app } from "../app";
 import request from "supertest";
+import { Client } from "pg";
+
+import { AppDataSource } from '../database/dataSource';
+
+beforeAll(async () => {
+  // connect with DB before tests
+  await AppDataSource.initialize();
+});
+
+afterAll(async () => {
+  // desconect from database after tests
+  await AppDataSource.destroy(); 
+});
 
 describe("Register user controller", () => {
 
@@ -11,7 +24,24 @@ describe("Register user controller", () => {
       password: "tester123"
     };
 
-    const response = request(app).post('/register').send(data);
+    const response = await request(app).post('/register').send(data);
+    
+    const { password, ...userData } = data;
+    const { id: _, ...userResponse } = response.body;
+
+    const client = new Client({
+      host: "localhost",
+      port: 5432,
+      password: "12345678",
+      application_name: "postgres",
+      user: "postgres",
+      database: "postgres"
+    });
+    await client.connect();
+    await client.query('DELETE FROM users;');
+
+    expect(userResponse).toStrictEqual(userData);
+
   })
 
 })
