8f6eef37973b504cf4bebc43d3278f1157172edd
Matosoli <guilhermematosgto2015@outlook.com>
2023-09-28 16:39:27 -0300

    formatChat functipon added :rocket:

---
+++ Server/src/utils/formatChat.ts
@@ -0,0 +1,12 @@
+import { Chat } from "../database/entities/Chat";
+
+
+export function formatChats( user: string, chats: Chat[] ){
+
+  const chatList = chats.map(chat => {
+    
+    
+
+  });
+
+};
