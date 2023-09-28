1746d9a6291d89364e89404c9c99c43c57d29466
Matosoli <guilhermematosgto2015@outlook.com>
2023-09-28 16:29:52 -0300

    change listchatservice to return users informations :rocket:

--- Server/src/services/ListChatService.ts
+++ Server/src/services/ListChatService.ts
@@ -1,12 +1,14 @@
 import { IChatRepository } from "../repositories/in-memory/ChatRepositoryTest";
 
 export class ListChatService{
-  constructor( private chatRepository: Omit<IChatRepository, "chats"> ){  }
+  constructor( private chatRepository: Omit<IChatRepository, "chats"> ){};
 
   async list( { username } ){
 
     const chats = await this.chatRepository
     .createQueryBuilder('chat')
+    .leftJoinAndSelect('chat.userSender', 'userSender')
+    .leftJoinAndSelect('chat.userReceiver', 'userReceiver')
     .where('chat.userReceiver = :username OR chat.userSender = :username', { username })
     .getMany();
 
