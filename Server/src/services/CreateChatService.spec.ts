0eb1e9091fe8a05bff365db3538099df8da872c2
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-05-04 16:50:07 -0300

    finish createchatservice unittest :rocket:

--- Server/src/services/CreateChatService.spec.ts
+++ Server/src/services/CreateChatService.spec.ts
@@ -1,14 +1,49 @@
 import { ChatRepositoryTest } from "../repositories/in-memory/ChatRepositoryTest";
+import { UserRepositoryTests } from "../repositories/in-memory/UserRepositoryTests";
+
+import { RegisterUserService } from "./RegisterUserService";
 import { CreateChatService } from "./CreateChatService";
 
 describe("CreateChatService", () => {
+  let repository;
   let chatService;
+  let registerservice;
 
   beforeAll(() => {
-    chatService = new CreateChatService(ChatRepositoryTest);
+    repository = UserRepositoryTests;
+    chatService = new CreateChatService(ChatRepositoryTest, repository);
+    registerservice = new RegisterUserService(repository);
   })
 
   it("Should be able to create a new chat", async () => {
+    const userSender = { 
+      name: "Testing",
+      username: "abec",
+      email: "testinasasssssddddddsg@a.com",
+      password: "testing123"
+    };
+
+    const userReceiver = { 
+      name: "Testing",
+      username: "abecas",
+      email: "tasestasdasiang@s.com",
+      password: "testing123"
+    };
+
+    await registerservice.register(userSender);
+    await registerservice.register(userReceiver);
+
+    const data = {
+      userSender: "abec",
+      userReceiver: "abecas"
+    };
+
+    const chat = await chatService.create(data);
+
+    expect(chat).toHaveProperty("id");
+  });
+
+  it("Shouldn't be able to create a new chat", async () => {
     const data = {
       userSender: "grugo",
       userReceiver: "Jeff"
@@ -16,6 +51,6 @@ describe("CreateChatService", () => {
 
     const chat = await chatService.create(data);
 
-    expect(chat).toHaveProperty("id")
+    expect(chat).toEqual("Can not find user sender" || "Can not find user receiver")
   })
 })
