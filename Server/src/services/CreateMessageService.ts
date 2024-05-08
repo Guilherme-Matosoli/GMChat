080d29cfdd82ab1da22c6ddaea29694ee3994e46
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-05-08 16:05:29 -0300

    finish CreateMessageService :rocket:

--- Server/src/services/CreateMessageService.ts
+++ Server/src/services/CreateMessageService.ts
@@ -2,6 +2,7 @@ import { Repository } from "typeorm";
 import { Message } from "../database/entities/Message";
 import { Chat } from "../database/entities/Chat";
 import { User } from "../database/entities/User";
+import { genChatId } from "../utils/genChatId";
 
 
 export class CreateMessageService{
@@ -14,8 +15,20 @@ export class CreateMessageService{
 
       const chatExists = await this.chatRepository.findOne({ where: { id: chatId } });
       if(!chatExists) return "Chat doesn't exists";
-      
 
+      const time = new Date();
+      const brazilDate = time.toLocaleString('en-US', { timeZone: 'America/Sao_Paulo' })
+
+      const message = await this.messageRepositry.create({
+        id: genChatId(),
+        sender,
+        chatId,
+        time: brazilDate
+      });
+
+      await this.messageRepositry.save(message);
+
+      return message;
     }
     catch(err){
       console.log(err)
