45644ccc6dda6e41e197112049a33b8546ad38b5
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-05-19 22:48:49 -0300

    if token is invalid auth middleware returns 'invalid token' message :bulb:

--- Server/src/middlewares/authMiddleware.ts
+++ Server/src/middlewares/authMiddleware.ts
@@ -3,15 +3,21 @@ import JWT, { JwtPayload } from "jsonwebtoken";
 import { UserRepository } from "../repositories/UserRepository";
 
 export const authMiddleware = async ( req: Request, res: Response, next: NextFunction ) => {
-  const { authorization } = req.headers;
-  if(!authorization) return res.status(401).json({ message: "User without authorization" });
-
-  const jwt = authorization.split(' ')[1];
-  const { username } = JWT.verify(jwt, process.env.JWT_PASS || '') as JwtPayload;
-
-  const user = await UserRepository.findOne({where: { username }});
-  if(!user) return res.status(404).json({ message: "User not found" });
-
-  req.headers.cookie = JSON.stringify(user);
-  next();
+  try{
+    const { authorization } = req.headers;
+    if(!authorization) return res.status(401).json({ message: "User without authorization" });
+  
+    const jwt = authorization.split(' ')[1];
+    const { username } = JWT.verify(jwt, process.env.JWT_PASS || '') as JwtPayload;
+  
+    const user = await UserRepository.findOne({where: { username }});
+    if(!user) return res.status(404).json({ message: "User not found" });
+  
+    req.headers.cookie = JSON.stringify(user);
+    next();
+  }
+  catch(err){
+    console.log(err);
+    res.status(401).json({ message: "Invalid token" })
+  }
 };
