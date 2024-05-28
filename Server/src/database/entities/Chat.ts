c5ac8506e8aa6baad523603b4523f5dd5b5e1f25
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-05-28 16:54:06 -0300

    merge

--- Server/src/database/entities/Chat.ts
+++ Server/src/database/entities/Chat.ts
@@ -1,6 +1,5 @@
-import { Entity, JoinColumn, ManyToOne, OneToMany, PrimaryColumn } from "typeorm";
+import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import { User } from "./User";
-import { Message } from "./Message";
 
 @Entity({ name: 'chats', schema: 'public' })
 export class Chat {
