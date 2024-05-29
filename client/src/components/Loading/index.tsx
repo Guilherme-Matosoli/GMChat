ad238e27ee18cbb6244c36276158a306cc2c6cdf
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-05-29 18:19:04 -0300

    add loading component :green_heart:

---
+++ client/src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import { LoadingIcon } from "../LoadingIcon";
+import { Container } from "./styles";
+
+
+export const Loading = () => {
+  return (
+    <Container>
+      <LoadingIcon />
+    </Container>
+  )
+};
