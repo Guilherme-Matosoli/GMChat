997e3daec8c3c4905326c179dd5e82c6dda83b65
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-01 00:30:19 -0300

    add deletechatservice :rotating_light:

---
+++ Server/src/services/Chat/DeleteChatService.ts
@@ -0,0 +1,25 @@
+import { Repository } from "typeorm";
+import { Chat } from "../../database/entities/Chat";
+import { Message } from "../../database/entities/Message";
+import { DeleteMessageService } from "../Message/DeleteMessageService";
+
+interface DelChatProps {
+  chatId: string,
+  username: string
+}
+
+export class DeleteChatService {
+  constructor(private chatRepository: Repository<Chat>, private messageRepository: Repository<Message>) { };
+
+  async delete({ chatId, username }: DelChatProps) {
+    try {
+      const delMessages = await new DeleteMessageService(this.messageRepository, this.chatRepository).delete({ chatId, username });
+      if (delMessages == "User is not in chat.") return delMessages;
+
+      await this.chatRepository.delete({ id: chatId })
+    }
+    catch (err) {
+      console.log(err)
+    }
+  };
+};
