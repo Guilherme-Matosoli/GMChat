85a2ba750f5b510b08c42b0adad04f89216be9ab
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-11 17:12:39 -0300

    add an instance of delete chat service on deleteChatService tests :fire:

--- Server/src/services/Chat/DeleteChatService.spec.ts
+++ Server/src/services/Chat/DeleteChatService.spec.ts
@@ -1,3 +1,16 @@
-describe("DeleteChatService", () => {
+import { ChatRepositoryTest } from "../../repositories/in-memory/ChatRepositoryTests";
+import { MessageRepositoryTest } from "../../repositories/in-memory/MessageRepositoryTests";
+import { DeleteChatService } from "./DeleteChatService";
 
+describe("DeleteChatService", () => {
+  let deleteChatService: DeleteChatService;
+
+  beforeAll(() => {
+    deleteChatService = new DeleteChatService(ChatRepositoryTest, MessageRepositoryTest);
+  });
+
+
+  it("Should be able to delete a chat", () => {
+
+  });
 });
