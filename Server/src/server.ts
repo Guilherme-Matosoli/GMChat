57d813e51dda9761d6c843ad2c4b45e4c0cd4b1a
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-21 00:59:07 -0300

    change render antisleep route again :robot:

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -15,7 +15,7 @@ export const io = new Server(serverHttp, {
 
 const initialize = async () => {
   setInterval(() => {
-    fetch("https://gmchat-a91m.onrender.com/antisleep")
+    fetch("https://gmchat-jt88.onrender.com/antisleep")
   }, 30000)
 
   await AppDataSource.initialize();
